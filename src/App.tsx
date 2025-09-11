f7d7898d4846e805b420711a31857cbd81501eeb
Abhijit Nath <abhijitnath1a1@gmail.com>
2025-09-11 23:23:10 +0530

    Darkmode Toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
+import { ThemeProvider } from './context/theme-provider';
+import WeatherDashboard from './pages/weather-dashboard';
+import CityPage from './pages/city-page';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Layout>Koti mar</Layout>
+      <ThemeProvider defaultTheme='dark'>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<WeatherDashboard />} />
+            <Route path='/city/:cityName' element={<CityPage />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,15 @@
 import { HeartIcon } from 'lucide-react';
 import type { PropsWithChildren } from 'react';
+import Header from './Header';
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className='bg-gradient-to-br from-background to-muted'>
-      header
+      <Header />
       <main className='min-h-screen container mx-auto px-4 py-8'>
         {children}
       </main>
-      <footer className='border-t backdrop-blur py-12'>
+      <footer className='border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60'>
         <div className='container mx-auto px-4 text-center text-gray-400'>
           <p className='flex items-center justify-center gap-1'>
             Made with <HeartIcon className='inline h-4 w-4 text-red-500' /> by
