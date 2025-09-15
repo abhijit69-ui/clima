baa5d336f2a116e5c0d837714b639e931ab568d1
Abhijit Nath <abhijitnath1a1@gmail.com>
2025-09-15 13:44:37 +0530

    Updated footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,8 +12,9 @@ const Layout = ({ children }: PropsWithChildren) => {
       <footer className='border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60'>
         <div className='container mx-auto px-4 text-center text-gray-400'>
           <p className='flex items-center justify-center gap-1'>
-            Made with <HeartIcon className='inline h-4 w-4 text-red-500' /> by
-            Abijit
+            made with{' '}
+            <HeartIcon className='inline h-4 w-4 text-red-500 fill-current' />{' '}
+            by abijit
           </p>
         </div>
       </footer>
